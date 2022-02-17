e62f93a0585168a03b43a5cded0798c684cec6ef
Gilson-Dourado10 <gilsondourado10@gmail.com>
2022-02-17 06:02:58 -0300

    Cronometro com  c#

---
+++ Cronometro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start();
+        }
+
+        static void start()
+        {
+            int tempo = 10;
+            int minutoAtual = 0;
+            while (minutoAtual != tempo)
+            {
+               minutoAtual++;
+               Console.WriteLine(minutoAtual);
+            }
+        }
+        
+    }
+}
